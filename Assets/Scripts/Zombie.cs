bbc259c9b7610289073ed888b7c486e00c76f561
LuisMiguelRobles <luismirobles97@gmail.com>
2019-12-06 21:51:14 -0500

    version1.0

---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : MonoBehaviour
+{
+    public Transform character;
+    public float velocity;
+
+    // Start is called before the first frame update4
+    void Start()
+    {
+        character = GameObject.Find("Character").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(transform.forward * velocity);
+        transform.LookAt(character);
+        
+    }
+
+    public void Killer()
+    {
+        Destroy(this.gameObject);
+    }
+}
